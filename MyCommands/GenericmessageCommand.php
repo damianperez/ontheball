b2563882c9c3eddcb3eb029cb54451f84f123200
Damian Perez <perezdamian@gmail.com>
2026-07-17 12:02:17 -0300

    Congig

--- MyCommands/GenericmessageCommand.php
+++ MyCommands/GenericmessageCommand.php
@@ -30,6 +30,20 @@ class GenericmessageCommand extends SystemCommand
      */
     public function execute(): ServerResponse
     {
+        $message = $this->getMessage();
+        // If a conversation is busy, execute the conversation command after handling the message.
+        $conversation = new Conversation(
+            $message->getFrom()->getId(),
+            $message->getChat()->getId()
+        );
+        // Fetch conversation command if it exists and execute it.
+        if ($conversation->exists() && $command = $conversation->getCommand()) {
+            return $this->telegram->executeCommand($command);
+        }
+        $message_text = $message->getText(true);
+        if ($message_text=='sarasa')            
+            $this->debug_a_admins('Respuesta',json_encode($this->replyToChat('escribieron sarasa')));            
+
         $web_app_data = $this->getMessage()->getWebAppData();
         if ($web_app_data) {
             return $this->replyToChat(
