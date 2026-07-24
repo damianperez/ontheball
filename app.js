806dfb09c5ca1155a1b265bfa2e035ed9357ab4f
Damian Perez <perezdamian@gmail.com>
2026-07-24 12:53:37 -0300

    servicio

--- app.js
+++ app.js
@@ -363,7 +363,7 @@ function startPolling() {
     );
     APP.polling = setInterval(
         consultarEstado,
-        APP.pollingSeconds * 1000   //decia 1000 antes ms
+        APP.pollingSeconds * 2500   //decia 1000 antes ms
     );
 }
 /* ========================================================== */
