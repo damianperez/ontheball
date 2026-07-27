6f94cfc95f762cf91b206c25c4bc6f3b7646c8f6
Damian Perez <perezdamian@gmail.com>
2026-07-27 13:56:46 -0300

    servicio

--- bot.php
+++ bot.php
@@ -93,7 +93,7 @@ try {
     */
         TelegramClient::sendMessage(
             $update["message"]["chat"]["id"],
-            $data["form"]["nombre"] . " " . $data["form"]["mensaje"],
+            'recibido: ' . $data["form"]["nombre"] . " " . $data["form"]["mensaje"],
             [
                 "reply_markup" =>
                 json_encode(
